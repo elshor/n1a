83140e62220bcea78b87bf630c1abc1e688604f7
Elchanan Shor <elshor@gmail.com>
2023-06-30 04:59:41 +0300

    writing script

--- run.sh
+++ run.sh
@@ -1,1 +1,11 @@
-accelerate launch /workspace/axolotl/scripts/finetune.py /root/n1a/config2.yml
+pip3 install --upgrade nvitop &
+git clone https://github.com/elshor/n1a.git
+git clone https://github.com/OpenAccess-AI-Collective/axolotl
+cd /root/n1a
+python -m venv env
+source env/bin/activate
+cd ../axolotl
+pip3 install -e .
+pip3 install -U git+https://github.com/huggingface/peft.git
+cd /root/n1a
+python finetune.py config2.yml
